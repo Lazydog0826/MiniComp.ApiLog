1b22220a644fff8ee4ab02ec7f859c7505a5223a
huangzicheng <770233343@qq.com>
2025-06-06 14:48:41 +0800

    feat: 新增文件日志事件

--- MiniComp.ApiLog/MiniComp.ApiLog/Setup.cs
+++ MiniComp.ApiLog/MiniComp.ApiLog/Setup.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MiniComp.Core.App;
+using MiniComp.Core.Extension;
 using Newtonsoft.Json;
 using Spectre.Console;
 using Spectre.Console.Json;
@@ -7,6 +9,8 @@ namespace MiniComp.ApiLog;
 
 public static class Setup
 {
+    private static readonly Lock Lock = new Lock();
+
     public static IServiceCollection AddApiLog(this IServiceCollection services)
     {
         services.AddScoped<IApiLogService, ApiLogService>();
@@ -20,5 +24,28 @@ public static class Setup
             new Panel(json).Header("异常日志").Collapse().RoundedBorder().BorderColor(Color.Red)
         );
         await Task.CompletedTask;
+    };
+
+    public static RecordLogDelegate WriteLogFile = async model =>
+    {
+        await Task.CompletedTask;
+        lock (Lock)
+        {
+            var rootPath = Path.Join(HostApp.AppRootPath, "logs");
+            var fileName = DateTimeExtension.Now().ToString("yyyy-MM-dd") + ".log";
+            if (Directory.Exists(rootPath))
+            {
+                Directory.CreateDirectory(rootPath);
+            }
+            var filePath = Path.Join(rootPath, fileName);
+            File.AppendAllTextAsync(
+                filePath,
+                $"{DateTimeExtension.Now():yyyy-MM-dd HH:mm:ss}===================="
+                    + Environment.NewLine
+                    + JsonConvert.SerializeObject(model)
+                    + $"======================================="
+                    + Environment.NewLine
+            );
+        }
     };
 }
