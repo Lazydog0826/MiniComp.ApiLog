593084b2688cdf6196090838bcea6c58eb2e0d17
hzc <2635174704@qq.com>
2025-06-18 18:18:57 +0800

    fix: 修复bug

--- MiniComp.ApiLog/MiniComp.ApiLog/ApiLogService.cs
+++ MiniComp.ApiLog/MiniComp.ApiLog/ApiLogService.cs
@@ -12,7 +12,7 @@ using Yitter.IdGenerator;
 
 namespace MiniComp.ApiLog;
 
-public class ApiLogService : IApiLogService
+public class ApiLogService(IOptions<RecordLogEvent> options) : IApiLogService
 {
     private readonly ApiLogModel _apiLogModel = new();
     private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
@@ -21,33 +21,30 @@ public class ApiLogService : IApiLogService
     private HttpStatusCode _httpStatusCode;
     private readonly HttpContext? _httpContext = WebApp.HttpContext;
     private IDictionary<string, object?> _request = new Dictionary<string, object?>();
-    private readonly RecordLogEvent _recordLogEvent;
-
-    public ApiLogService(IOptions<RecordLogEvent> options)
-    {
-        _recordLogEvent = options.Value;
-    }
+    private readonly RecordLogEvent _recordLogEvent = options.Value;
 
     public ApiLogModel GetApiLogModel() => _apiLogModel;
 
     public async Task SaveApiLogAsync()
     {
         _stopwatch.Stop();
-        var controllerActionDescriptor = _httpContext?.GetMetaData<ControllerActionDescriptor>();
-        if (_logIsActive == false || _httpContext == null || controllerActionDescriptor == null)
+        if (_logIsActive == false)
+        {
             return;
+        }
 
+        var controllerActionDescriptor = _httpContext?.GetMetaData<ControllerActionDescriptor>();
         _apiLogModel.BeginTime = DateTimeExtension.Now();
-        _apiLogModel.ServerIp = _httpContext.GetServerIp();
-        _apiLogModel.ClientIp = _httpContext.GetClientIp();
-        _apiLogModel.Path = _httpContext.Request.Path.ToString();
-        _apiLogModel.CodePath = controllerActionDescriptor.DisplayName ?? string.Empty;
-        _apiLogModel.Method = _httpContext.Request.Method;
-        _apiLogModel.RequestHeaders = JsonConvert.SerializeObject(_httpContext.Request.Headers);
+        _apiLogModel.ServerIp = _httpContext?.GetServerIp() ?? string.Empty;
+        _apiLogModel.ClientIp = _httpContext?.GetClientIp() ?? string.Empty;
+        _apiLogModel.Path = _httpContext?.Request.Path.ToString() ?? string.Empty;
+        _apiLogModel.CodePath = controllerActionDescriptor?.DisplayName ?? string.Empty;
+        _apiLogModel.Method = _httpContext?.Request.Method ?? string.Empty;
+        _apiLogModel.RequestHeaders = JsonConvert.SerializeObject(_httpContext?.Request.Headers);
         _apiLogModel.EndTime = DateTimeExtension.Now();
         _apiLogModel.ElapsedTime = _stopwatch.ElapsedMilliseconds;
         _apiLogModel.Parameter = JsonConvert.SerializeObject(_request);
-        _apiLogModel.ResponseHeaders = JsonConvert.SerializeObject(_httpContext.Response.Headers);
+        _apiLogModel.ResponseHeaders = JsonConvert.SerializeObject(_httpContext?.Response.Headers);
 
         try
         {
@@ -66,14 +63,6 @@ public class ApiLogService : IApiLogService
                     .BorderColor(Color.Red)
             );
         }
-        finally
-        {
-            if (_responseContent != null)
-            {
-                _httpContext.Response.StatusCode = (int)_httpStatusCode;
-                await _httpContext.Response.WriteAsJsonAsync(_responseContent);
-            }
-        }
     }
 
     public void SetExceptionResponseResult(
@@ -87,6 +76,11 @@ public class ApiLogService : IApiLogService
         _apiLogModel.Exception = ex;
         _responseContent = resObj;
         _httpStatusCode = httpStatusCode;
+        if (_httpContext != null)
+        {
+            _httpContext.Response.WriteAsJsonAsync(resObj);
+            _httpContext.Response.StatusCode = (int)_httpStatusCode;
+        }
     }
 
     public void SetRequest(IDictionary<string, object?> obj)
